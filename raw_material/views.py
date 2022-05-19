247a81693dd5aa8342c2db1c5f4e6f29961a2446
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-05-19 23:08:11 +0200

    same --init-- and path

--- raw_material/views.py
+++ raw_material/views.py
@@ -1,4 +1,4 @@
-from asyncio.windows_events import NULL
+# from asyncio.windows_events import NULL
 from django.shortcuts import render, get_object_or_404, redirect 
 from django.utils import timezone
 from .models import WareTypes, WareData, ProductIngredient, ProductAcquisition
