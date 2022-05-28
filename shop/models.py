5fdd6c67eb8c814b62ab11bdf45db308dc526548
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-05-28 09:54:57 +0200

    new modell CoffeeOrder

--- shop/models.py
+++ shop/models.py
@@ -15,3 +15,16 @@ class CoffeeMake(models.Model):
         default=User)
     c_make_date = models.DateTimeField('Elkészül:', null=True, blank=True)
     c_reg_time = models.DateTimeField('Rögzítés dátuma', default=timezone.now)
+
+class CoffeeOrder(models.Model):
+    coffe_selected = models.ForeignKey(CoffeeMake, on_delete=models.CASCADE, verbose_name='Lefoglalt Kávé')
+    coffee_dose = models.IntegerField(verbose_name='Kávé Adag', default=1)
+    sugar_dose =  models.IntegerField(verbose_name='Cukor Adag', default=1)
+    milk_dose =  models.IntegerField(verbose_name='Tej Adag', default=1)
+    coffe_user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, 
+        null=True, blank=False, 
+        related_name ='Lefoglalta', 
+        verbose_name = 'Lefoglalta',
+        default=User)
+    coffee_reg = models.DateTimeField('Rögzítés dátuma', default=timezone.now)
