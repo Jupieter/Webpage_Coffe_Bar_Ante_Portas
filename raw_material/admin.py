257612b6d83eaafbfe872b8d89666527554f44d9
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-06-01 16:31:38 +0200

    WareTypes  adminModel

--- raw_material/admin.py
+++ raw_material/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from .models import WareTypes, WareData, ProductIngredient, ProductAcquisition
 
 
-@admin.register(WareData)
+@admin.register(WareTypes)
+class WareTypesAdmin(admin.ModelAdmin):
+  list_display = ('id', 'ware_types')
+
 class WareDataAdmin(admin.ModelAdmin):
   list_display = ('id', 'ware_type', 'ware_brand', 'ware_brand_name', 'ware_weight', 'ware_price', 'pub_date')
 
-admin.site.register(WareTypes)
 admin.site.register(ProductIngredient)
 admin.site.register(ProductAcquisition)
