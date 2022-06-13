cce47e1522b1945f2951e829a257c408bdc2c2df
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-06-13 15:40:22 +0200

    indications for cooking and accounting4

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from django.shortcuts import render
 # from .forms import ContactForm
 from django.http import JsonResponse, HttpResponse
@@ -11,9 +12,10 @@ def home_page(request):
     dt_start = dt
     dt_end = dt + datetime.timedelta(hours=36)
     coffees1 = CoffeeMake.objects.filter(c_make_date__range =(dt_start, dt_end))
+    coffees2 = CoffeeMake.objects.filter(c_book__isnull=True)
     coffees = coffees1.order_by('c_make_date')
     return render(request, "home.html", {'sub_site_logo':sub_site_logo, 
-        'coffees':coffees,'dt':dt, 'dt_end':dt_end,})
+        'coffees':coffees,'coffees2':coffees2,})
 
 def contact_page(request):
     sub_site_logo = "src=static/image/coffe_bean_heart.png"
