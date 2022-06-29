f7896198b393c264a51f72c4642252600c3e46ec
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-06-29 17:05:45 +0200

    Order booking MODAL design

--- static/js/c_order_form.js
+++ static/js/c_order_form.js
@@ -1,5 +1,4 @@
 function re_fresh(Rng, Lbl) {
-  var bttn = document.getElementById('save_btn');
   var slider = document.getElementById(Rng);
   var output = document.getElementById(Lbl);
   output.innerHTML = slider.value;
