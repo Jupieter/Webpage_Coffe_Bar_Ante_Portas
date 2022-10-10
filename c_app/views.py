d92782f382d8d7a362c4124bdd8ddd37572d9296
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-10-10 15:12:20 +0200

    new order request to app

--- c_app/views.py
+++ c_app/views.py
@@ -23,9 +23,20 @@ from  .models import *
 
 def coffee_order_data():
     wares = ProductAcquisition.objects.filter(store_status=3)
+    coffee = []
     sugar = [json.dumps({"type_id":2, "w_id":0, "w_name":"Zero Sugar", "w_dose":0})]
     milk = [json.dumps({"type_id":2, "w_id":0, "w_name":"Zero Milk", "w_dose":0})]
     flavour = [json.dumps({"type_id":3, "w_id":0, "w_name":"Zero Flavour", "w_dose":0})]
+    tasks = dt_coffee_make(60)
+    print(tasks)
+    for task in tasks:
+            print(task.c_make_ware.id)
+            print(task.id)
+            print(task.c_make_ware)
+            print(task.c_make_dose)
+            i = {"type_id": 1, "w_id":task.id, "w_name":str(task.c_make_ware), "w_dose":int(task.c_make_dose)}
+            i_json = json.dumps(i)
+            coffee.append(i_json)
     for ware in wares:
         if ware.ware_type.ware_type.id == 2:
             type_id = ware.ware_type.ware_type.id 
@@ -57,12 +68,12 @@ def coffee_order_data():
             i = {"type_id":type_id, "w_id":w_id, "w_name":w_name, "w_dose":w_dose}
             i_json = json.dumps(i)
             flavour.append(i_json)
-    return sugar, milk, flavour
+            tastes = [coffee, sugar, milk, flavour]
+    return tastes
 
 @api_view(['GET'])
 def c_order_data(request):
-    sgr, mlk, flvr = coffee_order_data()
-    data = [sgr, mlk, flvr]
+    data = coffee_order_data()
     return Response(data, status=status.HTTP_200_OK)
 
 def dt_coffee_make(hourS=1):
@@ -145,7 +156,7 @@ def coffee_notify(request):
 
 @api_view(['GET'])
 def todaytcoffee(request):
-    tasks = dt_coffee_make(20)
+    tasks = dt_coffee_make(60)
     # print(tasks)
     serializer = FirstCoffeeSerializer(tasks, many=True)
     # print(serializer.data)
