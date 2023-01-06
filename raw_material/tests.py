bf80e49341619cca2ef551908d61d001d5196416
Jupieter <peterjung777@gmail.com>
2023-01-06 18:04:46 +0100

    test database loader to DB_Creator class

--- raw_material/tests.py
+++ raw_material/tests.py
@@ -2,7 +2,10 @@ from django.test import TestCase, SimpleTestCase, Client
 from django.urls import reverse, resolve
 from raw_material.views import ware_choice, ware_list, ware_new
 from raw_material.urls import *
-from raw_material.models import WareTypes
+from raw_material.models import WareTypes, WareData, ProductAcquisition
+from accounts.models import User
+from shop.models import CoffeeMake, CoffeeOrder
+from tests.test_model import DB_Creator
 
 
 
@@ -15,57 +18,57 @@ class Test_Raw_material_Urls(TestCase):
         )
 
 
-    def test_1_ware_choice_url_resolves(self):
+    def test_01_ware_choice_url_resolves(self):
         url = reverse('raw_material:ware_choice')
         self.assertEqual(resolve(url).func, ware_choice)
         self.assertEqual(resolve(url).route, "raw_material/")
 
-    def test_2_ware_new_url_resolves(self):
+    def test_02_ware_new_url_resolves(self):
         url = reverse('raw_material:ware_new')
         self.assertEqual(resolve(url).func, ware_new)
         self.assertEqual(resolve(url).route, "raw_material/new/")
 
-    def test_3_ware_list_url_resolves(self):
+    def test_03_ware_list_url_resolves(self):
         url = "/raw_material/(1)/"
         self.assertEqual(resolve(url).func, ware_list)
         self.assertEqual(resolve(url).route, "raw_material/(<int:pk>)/")
 
-    def test_4_ware_edit_url_resolves(self):
+    def test_04_ware_edit_url_resolves(self):
         url = "/raw_material/(1)/(2)/edit/"
         self.assertEqual(resolve(url).func, ware_edit)
         self.assertEqual(resolve(url).route, 'raw_material/(<int:pk>)/(<int:pkey>)/edit/')
 
-    def test_5_acquisition_new_url_resolves(self):
+    def test_05_acquisition_new_url_resolves(self):
         url = "/raw_material/(1)/(2)/acquisition_new/"
         self.assertEqual(resolve(url).func, acquisition_new)
         self.assertEqual(resolve(url).route, 'raw_material/(<int:pk>)/(<int:pkey>)/acquisition_new/')
 
-    def test_5_acquisition_new_url_resolves(self):
+    def test_06_acquisition_new_url_resolves(self):
         url = "/raw_material/acquisition_list/"
         self.assertEqual(resolve(url).func, acquisition_list)
         self.assertEqual(resolve(url).route, 'raw_material/acquisition_list/')
 
-    def test_6_acquisition_remove_url_resolves(self):
+    def test_07_acquisition_remove_url_resolves(self):
         url = "/raw_material/acquisition_list/(1)/remove/"
         self.assertEqual(resolve(url).func, acquisition_remove)
         self.assertEqual(resolve(url).route, 'raw_material/acquisition_list/(<int:pkey>)/remove/')
 
-    def test_7_acquisition_edit_url_resolves(self):
+    def test_08_acquisition_edit_url_resolves(self):
         url = "/raw_material/acquisition_list/(1)/edit/"
         self.assertEqual(resolve(url).func, acquisition_edit)
         self.assertEqual(resolve(url).route, 'raw_material/acquisition_list/(<int:pkey>)/edit/')
 
-    def test_8_acquisition_storing_url_resolves(self):
+    def test_09_acquisition_storing_url_resolves(self):
         url = "/raw_material/acquisition_list/(1)/storing/"
         self.assertEqual(resolve(url).func, acquisition_storing)
         self.assertEqual(resolve(url).route, 'raw_material/acquisition_list/(<int:pkey>)/storing/')
 
-    def test_9_acquisition_box_open_url_resolves(self):
+    def test_10_acquisition_box_open_url_resolves(self):
         url = "/raw_material/acquisition_list/(1)/box_open/"
         self.assertEqual(resolve(url).func, box_open)
         self.assertEqual(resolve(url).route, 'raw_material/acquisition_list/(<int:pkey>)/box_open/')
 
-    def test_10_acquisition_box_empty_url_resolves(self):
+    def test_11_acquisition_box_empty_url_resolves(self):
         url = "/raw_material/acquisition_list/(1)/box_empty/"
         #   print("url: ", url)
         #   print("resolve(url)", resolve(url))
@@ -76,10 +79,41 @@ class Test_Raw_material_Urls(TestCase):
 class Tests_Raw_material_Views(TestCase):
 
     def setUp(self) -> None:
+        DB_Creator.full_DB_begin(self)
         self.client = Client()        
-        # self.urls_list = ['/raw_material/', '/contact/', '/shop/', '/accounts/login/', '/api/login/']
+        self.urls_200_list = [
+            ['', "ware_choice.html"], 
+            ['new/', "ware_edit.html"], 
+            ['(1)/',  "ware_list.html"],
+            ['(1)/(2)/edit/', "ware_edit.html"], 
+            ['(1)/(2)/acquisition_new/', "acquisition_new.html"],
+            ['acquisition_list/', "acquisition_list.html"],
+            ['acquisition_list/(1)/edit/', "acquisition_new.html"],
+            ]
+        self.urls_302_list = [
+            ['acquisition_list/(1)/storing/', "acquisition_list.html"],
+            ['acquisition_list/(1)/box_open/', "acquisition_list.html"],
+            ['acquisition_list/(1)/box_empty/', "acquisition_list.html"],
+            ['acquisition_list/(1)/remove/', "acquisition_list.html"],
+            ]
 
-    def  test_raw_material_page(self):
-        response = self.client.get("/raw_material/")
-        print("raw_material", response)
-        self.assertTemplateUsed(response, "raw_material/ware_choice.html")
+    def  test_12_raw_material_200_response(self):
+        # print("********************")
+        for url in self.urls_200_list:
+            long_url = "/raw_material/" + url[0]
+            test_url = "raw_material/" +  url[1]
+            #   print("raw_material:    ",long_url, "    ", test_url)
+            response = self.client.get(long_url)
+            #   print(response)
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed(response, test_url)
+
+    def  test_13_raw_material_302_response(self):
+        # print("********************")
+        for url in self.urls_302_list:
+            long_url = "/raw_material/" + url[0]
+            test_url = "raw_material/" +  url[1]
+            # print("raw_material:    ",long_url, "    ", test_url)
+            response = self.client.get(long_url)
+            #   print(response)
+            self.assertEqual(response.status_code, 302)
--- tests/test_pages.py
+++ tests/test_pages.py
@@ -36,92 +36,11 @@ class SeleniumTestCase(StaticLiveServerTestCase):
 class HomeTest(SeleniumTestCase):
 
     def setUp(self):
-        # options = webdriver.ChromeOptions()
-        # options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        # self.driver = webdriver.Chrome(options=options)
-        # self.driver.set_window_size(1920 ,1080)
-
-        # Staff User create
-        user_saff = User.objects.create_staff_user(
-        email = "staff@admin.com",
-        username = "STAFF",
-        password = "XYZ999",
-        first_name = "Stefi", 
-        last_name = "Stafi"
-        )
-        user_saff.save()
-        # User create
-
-        user_list = [
-        ["test@acquisition.com", "Acquisitor", "ABC001", "Ac", "Qu"],
-        ["test@store.com", "Storer", "ABC002", "St0", "Re"],
-        ["test@open.com", "Opener", "ABC003", "Op", "En"],
-        ["test@make.com", "Maker", "ABC004", "Ma", "Ker"],
-        ["test@drink.com", "Drinker", "ABC005", "Dr", "Ink"],
-        ]
-
-        for i in range(5): 
-            u_ser = DB_Creator.user_creator(self, user_list[i][0], user_list[i][1], user_list[i][2], user_list[i][3])
-            u_ser.save()
-
-        # WareTypes load with Data
-        ware_type_list = [ ["Coffee", 7, "coffee_bag.jpg"], ["Sugar", 50, "sugar.jpg"], ["Milk", 50, "milk.jpg"], ["Flavour", 50, "dijo.jpg"] ]
-        for ware_type_data in ware_type_list: 
-            wt_types = ware_type_data[0]
-            wt_wght = ware_type_data[1]
-            wt = DB_Creator.ware_type_creator(wt_types, wt_wght)
-            wt.ware_image = ware_type_data[2]
-            wt.save()
-        # WareData load with Data
-        ware_data_list = [
-            ["Brand_Coffee", "Brand_Name_Coffee", 250, 900,],
-            ["Brand_Sugar", "Brand_Name_Sugar", 100, 700,],
-            ["Brand_Milk", "Brand_Name_Milk", 500, 800,],
-            ["Brand_Flavour", "Brand_Name_Flavour", 450, 600,]
-            ]
-        for i in range(4): 
-            wt = WareTypes.objects.get(id=i+1)
-            wd = DB_Creator.ware_data_creator(wt, ware_data_list[i][0], ware_data_list[i][1], ware_data_list[i][2], ware_data_list[i][3])
-            wd.save()
-        # ProductAcquisition load with Data
-        price_list = [699, 399, 299, 599]
-        for i in range(4): 
-            ware_pa = WareData.objects.get(id=i+1)
-            u_ser = User.objects.get(id=1)
-            pa = DB_Creator.ware_product_creator(ware_pa, price_list[i], u_ser)
-            pa.store_user = User.objects.get(id=2)
-            pa.open_user = User.objects.get(id=3)
-            pa.save()
-        
-        cm = CoffeeMake.objects.create(
-            c_make_ware = ProductAcquisition.objects.get(id=1),
-            c_make_dose = 4,
-            c_make_user = User.objects.get(id=4),
-            c_make_date = datetime(2022, 2, 22, 6, 6, 6, 0)
-        )
-        cm.save()
-
-        co = CoffeeOrder.objects.create(
-            coffee_selected = cm,
-            coffee_dose = 0.5,
-            sugar_choice = ProductAcquisition.objects.get(id=2),
-            sugar_dose = 1.0,
-            milk_choice = ProductAcquisition.objects.get(id=3),
-            milk_dose = 2.0,
-            flavour_choice = ProductAcquisition.objects.get(id=4),
-            flavour_dose = 1.5,
-            coffe_user = User.objects.get(id=5),
-        )
-        co.save()
-
-        user_c = User.objects.all()
-        print("")
-        print("User:  ", user_c)
+        DB_Creator.full_DB_begin(self)
     
     # @skip
     def homepage_title(self):
         # self.driver.get('http://127.0.0.1:8000/')
-        # self.driver.get('http://127.0.0.1:64432/')
         self.driver.get(self.live_server_url)
         sleep(0.1)
         assert "CoffeeBar" in self.driver.title
@@ -170,12 +89,13 @@ class HomeTest(SeleniumTestCase):
     
     def click_four_ware_type(self):
         href_list = [
-            ['//a[@href="/raw_material/(1)"]', "Coffee list"],
-            ['//a[@href="/raw_material/(2)"]', "Sugar list"],
-            ['//a[@href="/raw_material/(3)"]', "Milk list"],
-            ['//a[@href="/raw_material/(4)"]',"Flavour list"],
+            ['//a[@href="/raw_material/(1)/"]', "Coffee list"],
+            ['//a[@href="/raw_material/(2)/"]', "Sugar list"],
+            ['//a[@href="/raw_material/(3)/"]', "Milk list"],
+            ['//a[@href="/raw_material/(4)/"]',"Flavour list"],
             ]
         for href_act in href_list:
+            print(href_act[0])
             variable_button = self.driver.find_element(by=By.XPATH, value = href_act[0])
             variable_button.click()
             assert href_act[1] in self.driver.page_source
@@ -186,7 +106,7 @@ class HomeTest(SeleniumTestCase):
             sleep(1)
 
     def add_new_ware(self):
-        coffee_button = self.driver.find_element(by=By.XPATH, value = '//a[@href="/raw_material/(1)"]')
+        coffee_button = self.driver.find_element(by=By.XPATH, value = '//a[@href="/raw_material/(1)/"]')
         coffee_button.click()
         new_button = self.driver.find_element(by=By.XPATH, value = '//a[@href="/raw_material/new/"]')
         new_button.click()
@@ -204,7 +124,6 @@ class HomeTest(SeleniumTestCase):
         sleep(2)
         submit_button.click()
         wd_1 = WareData.objects.get(id=5)
-        print("Ware Data",wd_1)
         self.assertEqual(wd_1.ware_brand, "Test_Brand_Coffee")
         self.assertEqual(wd_1.ware_brand_name, "Test_Name_Coffee")
         self.assertEqual(wd_1.ware_weight, 250)
@@ -214,12 +133,10 @@ class HomeTest(SeleniumTestCase):
 
     def test_0_all(self):
         self.homepage_title()
-        # self.homepage_to_contact()
-        # self.back_to_homepage()
+        self.homepage_to_contact()
+        self.back_to_homepage()
         self.login_staff()
         self.raw_material_menu_button()
-        wd_0 = WareTypes.objects.all()
-        print("Ware Type", wd_0)
         self.click_four_ware_type()
         self.add_new_ware()
 
